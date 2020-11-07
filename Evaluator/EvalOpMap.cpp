bdf85e5ee1f0382e7db06b7213caa9f64d4e415e
Lucas <lucasmeisl@hotmail.com>
2020-11-07 13:52:15 -0700

    Kinda works dont want to lose to hard drive failure

---
+++ Evaluator/EvalOpMap.cpp
@@ -0,0 +1,309 @@
+#include "Evaluator.hpp"
+#include <cmath>
+#include <random>
+
+#include <iostream>
+
+const float DegToRad = 0.01745329252;
+
+//sorted by decreasing string size (can't use a map)
+std::vector< std::pair<std::string, char> > EvalLoader::tokenStringReplaceVector =
+{
+	{"cotangent",	8},
+	{"cosecant",	6 },
+	{"secant", 		7 },
+	{"cosec",		6 },
+	{"cotan", 		8 },
+	{"asin", 		6 },
+	{"acos", 		7 },
+	{"atan", 		8 },
+	{"cot", 		8 },
+	{"csc",			6 },
+	{"sec", 		7 },
+	{"pow", 	   '^'},
+	{"sin",  		3 },
+	{"cos",  		4 },
+	{"tan",  		5 },
+	{"==",			0 },
+	{">=",			1 },
+	{"<=",			2 }
+};
+
+std::map<char, std::string> EvalLoader::reverseTokenMapToCFunction = 
+{
+	{0, "=="},
+	{1, ">="},
+	{2, "<="},
+	{3, "sinf"},
+	{4, "cosf"},
+	{5, "tanf"},
+	{6, "asinf"},
+	{7, "acosf"},
+	{8, "atanf"},
+	{9, "rand()%"},
+	{10, "-"}
+};
+
+std::map<char, int> EvalLoader::opPriorityMap
+{
+//	{')',  -1},//NEVER EVEN ON THE STACK
+	{ 0 ,	0},// ==
+	{ 1 ,	0},// >=
+	{ 2 ,   0},// <=
+	{'+',	1},
+	{'-',	1},
+	{'*',	2},
+	{'/',	2},
+	{'^',	3},
+	{ 3 ,   5},//unary operators 3-10
+	{ 4 ,   5},
+	{ 5 ,   5},
+	{ 6 ,   5},
+	{ 7 ,   5},
+	{ 8 ,   5},
+	{ 9 ,   5},
+	{ 10,   5},
+	{'(',	-1}//so will apply
+};
+
+std::map<void(*)(std::stack<float>&), std::string> EvalLoader::reverseOpMap = 
+{
+	{Evaluator::add							,"+"},
+    {Evaluator::subtract					,"-"},
+    {Evaluator::multiply					,"*"},
+    {Evaluator::divide						,"/"},
+    {Evaluator::raiseByExponent				,"^"},
+    {Evaluator::testIfEqual					,"==" },
+    {Evaluator::testIfGreaterThan			,">"},
+    {Evaluator::testIfLessThan				,"<"},
+	{Evaluator::testIfGreaterOrEqualThan	,">="},
+    {Evaluator::testIfLessThanOrEqual		,"<="},
+	{Evaluator::sin							,"sin"},
+    {Evaluator::cos							,"cos"},
+    {Evaluator::tan							,"tan"},
+	{Evaluator::cosecant					,"csc"},
+    {Evaluator::secant						,"sec"},
+    {Evaluator::cotangent					,"cot"},
+    {Evaluator::random						,"rnd"},
+	{Evaluator::negate						,"neg"}
+};
+/*
+std::map<void(*)(std::stack<float>&), char> reverseOpPriorityMap = 
+{
+	{Evaluator::add							,'+'},
+    {Evaluator::subtract					,'-'},
+    {Evaluator::multiply					,'*'},
+    {Evaluator::divide						,'/'},
+    {Evaluator::raiseByExponent				,'^'},
+    {Evaluator::testIfEqual					, 0 },
+    {Evaluator::testIfGreaterThan			,'>'},
+    {Evaluator::testIfLessThan				,'<'},
+	{Evaluator::testIfGreaterOrEqualThan	, 1 },
+    {Evaluator::testIfLessThanOrEqual		, 2 },
+	{Evaluator::sin							, 3 },
+    {Evaluator::cos							, 4 },
+    {Evaluator::tan							, 5 },
+	{Evaluator::cosecant					, 6 },
+    {Evaluator::secant						, 7 },
+    {Evaluator::cotangent					, 8 },
+    {Evaluator::random						, 9 },
+	{Evaluator::negate						, 10}
+};
+*/
+//uses ascii code range 0-31 (unprintable control characters)
+//for where the tokens do not have a direct single character equivalent
+std::map<char,void(*)(std::stack<float>&)> EvalLoader::binaryOpMap =
+{
+    {'+' , Evaluator::add},
+    {'-' , Evaluator::subtract},
+    {'*' , Evaluator::multiply},
+    {'/' , Evaluator::divide},
+    {'^' , Evaluator::raiseByExponent},
+    { 0  , Evaluator::testIfEqual},
+    {'>' , Evaluator::testIfGreaterThan},
+    {'<' , Evaluator::testIfLessThan},
+	{ 1  , Evaluator::testIfGreaterOrEqualThan},
+    { 2  , Evaluator::testIfLessThanOrEqual}
+};
+
+//associativity???  so far all right associative...
+std::map<char,void(*)(std::stack<float>&)> EvalLoader::unaryOpMap =
+{
+    { 3  , Evaluator::sin},
+    { 4  , Evaluator::cos},
+    { 5  , Evaluator::tan},
+	{ 6  , Evaluator::cosecant},
+    { 7  , Evaluator::secant},
+    { 8  , Evaluator::cotangent},
+    { 9  , Evaluator::random},
+	{ 10 , Evaluator::negate}
+};
+
+//Map reverse map??? Function -> opPrority
+
+void Evaluator::add(std::stack<float>& numberStack)// '+'
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	std::cout<<"adding "<<lhs<<" and "<<rhs<<std::endl;
+	numberStack.push(lhs + rhs);
+}
+
+void Evaluator::subtract(std::stack<float>& numberStack)//-
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	std::cout<<"subtracting "<<lhs<<" minus "<<rhs<<std::endl;
+	numberStack.push(lhs - rhs);
+}
+
+void Evaluator::multiply(std::stack<float>& numberStack)//*
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	std::cout<<"multiplying "<<lhs<<" and "<<rhs<<std::endl;
+	numberStack.push(lhs * rhs);
+}
+
+void Evaluator::divide(std::stack<float>& numberStack)// /
+{
+    float rhs = numberStack.top();
+    numberStack.pop();
+    float lhs = numberStack.top();
+    numberStack.pop();
+    std::cout<<"dividing "<<lhs<<" by "<<rhs<<std::endl;
+    //if(rhs = 0.f) numberStack.push
+    numberStack.push(lhs / rhs);
+}
+
+void Evaluator::raiseByExponent(std::stack<float>& numberStack)//^
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	std::cout<<"raising "<<lhs<<" to the power of "<<rhs<<std::endl;
+	numberStack.push(pow(lhs,rhs));
+}
+
+void Evaluator::sin(std::stack<float>& numberStack)//S
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the sin of "<<num<<std::endl;
+	numberStack.push(sinf(num * DegToRad));
+}
+
+void Evaluator::cos(std::stack<float>& numberStack)//C
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the cos of "<<num<<std::endl;
+	numberStack.push(cosf(num * DegToRad));
+}
+
+void Evaluator::tan(std::stack<float>& numberStack)//T
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the tan of "<<num<<std::endl;
+	numberStack.push(tanf(num * DegToRad));
+}//use op ... command style ?
+
+void Evaluator::cosecant(std::stack<float>& numberStack)//S
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the cosecant of "<<num<<std::endl;
+	numberStack.push(asinf(num * DegToRad));
+}
+//abs() |x|
+
+void Evaluator::secant(std::stack<float>& numberStack)//C
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the secant of "<<num<<std::endl;
+	numberStack.push(acosf(num * DegToRad));
+}
+
+void Evaluator::cotangent(std::stack<float>& numberStack)//T
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"taking the cotangent of "<<num<<std::endl;
+	numberStack.push(atanf(num * DegToRad));
+}
+
+void Evaluator::random(std::stack<float>& numberStack)//R
+{
+	int num = numberStack.top();
+	numberStack.pop();
+	if(num <= 0) numberStack.push(0.f);
+	else numberStack.push(rand()%num);
+}
+
+
+void Evaluator::negate(std::stack<float>& numberStack)// _0
+{
+	float num = numberStack.top();
+	numberStack.pop();
+	std::cout<<"negating "<<num<<std::endl;
+	numberStack.push(-num);
+}
+
+void Evaluator::testIfEqual(std::stack<float>& numberStack)//=
+{
+    float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	//std::cout<<"testing if "<<lhs<<" equals "<<rhs<<std::endl;
+	(roundf(lhs) == roundf(rhs)) ? numberStack.push(1) : numberStack.push(0);
+}
+
+void Evaluator::testIfGreaterThan(std::stack<float>& numberStack)//>
+{
+    float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	//std::cout<<"testing if "<<lhs<<" is greater than "<<rhs<<std::endl;
+	(roundf(lhs) > roundf(rhs)) ? numberStack.push(1) : numberStack.push(0);
+}
+
+void Evaluator::testIfLessThan(std::stack<float>& numberStack)//<
+{
+    float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	//std::cout<<"testing if "<<lhs<<" is less than "<<rhs<<std::endl;
+	(roundf(lhs) < roundf(rhs)) ? numberStack.push(1) : numberStack.push(0);
+}
+
+void Evaluator::testIfGreaterOrEqualThan(std::stack<float>& numberStack)
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	//std::cout<<"testing if "<<lhs<<" is greater than "<<rhs<<std::endl;
+	(roundf(lhs) >= roundf(rhs)) ? numberStack.push(1) : numberStack.push(0);
+}
+
+void Evaluator::testIfLessThanOrEqual(std::stack<float>& numberStack)
+{
+	float rhs = numberStack.top();
+	numberStack.pop();
+	float lhs = numberStack.top();
+	numberStack.pop();
+	//std::cout<<"testing if "<<lhs<<" is less than "<<rhs<<std::endl;
+	(roundf(lhs) <= roundf(rhs)) ? numberStack.push(1) : numberStack.push(0);
+}
---
+++ Evaluator/Evaluator.hpp
@@ -0,0 +1,183 @@
+#ifndef EVALUATOR_HPP
+#define EVALUATOR_HPP
+
+//#include "Exception.hpp"
+
+//#include "ValArray.hpp"//hehehe
+struct VarIndice
+{
+    VarIndice() : letterNum(0), paramNum(0) {}
+    VarIndice(unsigned int a, unsigned int b) : letterNum(a), paramNum(b) {}
+	unsigned int letterNum;
+	unsigned int paramNum;
+};
+
+#include <string>
+#include <stack>
+#include <map>
+#include <vector>
+
+class Number
+{
+public:
+	virtual const float getVal() const = 0;
+	virtual const float getVal(const float *v, const unsigned int depth) const = 0;
+	virtual ~Number(){}
+	virtual bool isConstant() = 0;
+	virtual bool isComplex() = 0;
+};
+
+class Constant : public Number
+{
+public:
+	Constant(float n):myVal(n){}
+	~Constant(){}
+	const float getVal() const {return myVal;}
+	const float getVal(const float *v, const unsigned int depth) const {return myVal;}
+	bool isConstant() override {return true;}
+	bool isComplex()  override {return false;}
+private:
+	const float myVal;
+};
+
+class LVariable : public Number
+{
+public:
+	LVariable(VarIndice vi) : m_varIndice(vi){}
+	~LVariable(){}
+	const float getVal() const {return 0.0f;}
+	const float getVal(const float *v, const unsigned int depth) const;
+	bool isConstant() override {return false;}
+	bool isComplex()  override {return false;}
+private:
+	VarIndice m_varIndice;
+};
+
+class Global : public Number
+{
+public:
+	Global(unsigned int i) : m_index(i){}
+	~Global(){}
+	const float getVal() const {return 0.0f;}
+	const float getVal(const float *v, const unsigned int depth) const;
+	bool isConstant() override {return false;}
+	bool isComplex()  override {return false;}
+private:
+	unsigned int m_index;
+};
+
+//can be made from an operator and a sign...
+//has a reverse map
+//DIDN"T I DO THIS EARLIER???
+//what is in the brackets is a 'number'
+//what is out is not
+//2x(5) -> simple
+//2x(1+y) -> 2x*1 + 2x*y -> not simple, just a complex
+class ComplexNumber : public Number
+{
+public:
+	ComplexNumber(std::vector<Number*> _numList, std::vector< void(*)(std::stack<float>&) > _opList, std::vector<bool> _isNextStepAPop)
+					: numList(_numList), opList(_opList), isNextStepAPop(_isNextStepAPop) {}
+	const float getVal() const {return 0.0f;}
+	const float getVal(const float *v, const unsigned int depth) const {return 0.0f;}
+	bool isConstant() override {return false;}
+	bool isComplex()  override {return true;}
+//private://?
+	std::vector<Number*> numList;
+	std::vector< void(*)(std::stack<float>&) > opList;
+	std::vector<bool> isNextStepAPop;
+};
+//ie, what is between parentheses
+
+class Evaluator;
+
+//almost have a loader class here too...
+class EvalLoader
+{
+public:
+    EvalLoader(){}
+    ~EvalLoader(){}
+    Evaluator* load(const std::string& expression, std::map<char, VarIndice>& varIndiceMap, unsigned int depth);
+    Evaluator* load(const float value);
+private:
+    std::string m_expression;
+
+	void printout(const std::vector<Number*> &numList, std::vector< void(*)(std::stack<float>&) > &opList, const std::vector<bool> &popList);
+
+    static std::map<char,void(*)(std::stack<float>&)> binaryOpMap;
+	static std::map<char,void(*)(std::stack<float>&)> unaryOpMap;
+	static std::vector<std::pair<std::string, char> > tokenStringReplaceVector;
+	static std::map<char, std::string> reverseTokenMapToCFunction;
+	static std::map<void(*)(std::stack<float>&), std::string> reverseOpMap;
+	static std::map<char, int> opPriorityMap;
+    bool simplifyBinary(void(*)(std::stack<float>&));
+	bool simplifyUnary (void(*)(std::stack<float>&));
+	void evaluate(std::stack<char>& stackOp);
+	bool nextOpHasPriority(char nextOp, char lastOp);
+	bool isAnOp(const char& op);
+	bool isUnary(const char& op);
+
+	std::stack<float> stackNum;
+
+	std::stack< std::vector<bool> > isNextStepAPopStack;//instruction list
+	std::stack< std::vector<Number*> > numListStack;//numbers used in expression
+	std::stack< std::vector< void(*)(std::stack<float>&) > > opListStack;
+};
+
+//Interface class
+class Evaluator
+{
+    friend class EvalLoader;
+public:
+    Evaluator(const std::string& exp, unsigned int depth) : arrayDepth(depth), expression(exp){}
+    virtual ~Evaluator(){}
+    virtual float evaluate(const float *v) = 0;//{return 0.f;}
+    const unsigned int arrayDepth;
+    const std::string expression;
+protected:
+	static void add(std::stack<float>&);
+	static void subtract(std::stack<float>&);
+	static void multiply(std::stack<float>&);
+	static void divide(std::stack<float>&);
+	static void raiseByExponent(std::stack<float>&);
+	static void sin(std::stack<float>&);
+	static void cos(std::stack<float>&);
+	static void tan(std::stack<float>&);
+	static void cosecant(std::stack<float>&);
+	static void secant(std::stack<float>&);
+	static void cotangent(std::stack<float>&);
+	static void random(std::stack<float>&);
+	static void negate(std::stack<float>&);
+	static void testIfEqual(std::stack<float>&);
+	static void testIfGreaterThan(std::stack<float>&);
+	static void testIfLessThan(std::stack<float>&);
+	static void testIfGreaterOrEqualThan(std::stack<float>&);
+	static void testIfLessThanOrEqual(std::stack<float>&);
+};
+
+//needs a valHolder for variables duh
+class SimpleEvaluator:public Evaluator
+{
+public:
+    SimpleEvaluator(const std::string& exp, unsigned int depth, Number* n):Evaluator(exp,depth),m_number(n){}
+    ~SimpleEvaluator(){delete m_number;}
+    float evaluate(const float *v){return m_number->getVal(v, arrayDepth);}
+private:
+    const Number* const m_number;
+};
+
+class ComplexEvaluator:public Evaluator
+{
+public:
+    ComplexEvaluator(const std::string& expression, unsigned int depth, std::vector<Number*>& numbers, std::vector< void(*)(std::stack<float>&) >& ops, std::vector<bool>& steps);
+    ~ComplexEvaluator();
+	float evaluate(const float *v);
+private:
+	std::stack<float> stackNum;
+
+	std::vector<Number*> m_numList;//numbers used in expression
+	std::vector< void(*)(std::stack<float>&) > opList;//operators used in expression
+	std::vector<bool> isNextStepAPop;//instruction list
+};
+
+#endif //EVALUATOR_HPP
