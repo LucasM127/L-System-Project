e159683dc791de3c1ed1a0fb21712ff0dd22ab65
Lucas <what_is_hotmail@hotmail.com>
2021-03-07 22:57:57 -0700

    Added Lib Folder

---
+++ Lib/AllLibs.hpp
@@ -0,0 +1,11 @@
+#ifndef ALL_LIBS_HPP
+#define ALL_LIBS_HPP
+
+#include "../Logger/Logger.hpp"
+#include "../Evaluator/EvalLoader.hpp"
+#include "../Parsing/LSFileParser.hpp"
+#include "../Containers/OstreamOperators.hpp"
+//LSystem one is last...
+#include "../LSystem/LSInterpreter.hpp"
+
+#endif
