7fbad32a46601efa8e365297c28962a8a0990c0e
Lucas <what_is_hotmail@hotmail.com>
2021-03-20 16:35:17 -0600

    Added basic example

---
+++ Examples/Anabaena/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+#include "../../ls.hpp"
+
+#include "../../Containers/LSFileData.hpp"
+#include "../../Parsing/LSFileParser.hpp"
+#include "../../Containers/OstreamOperators.hpp"
+
+//Example from
+//pdf "Modeling plant development with L-systems" pg 5
+//"L-system modeling the development of a vegetative segment ofAnabaena"
+
+//[][ ]
+class CellTurtle : public LSYSTEM::LSInterpreter
+{
+public:
+    const std::string getString() {return m_string;}
+    void reset() override
+    {
+        m_string.clear();
+    }
+    void interpret(const LSYSTEM::LModule &&M) override
+    {
+        if(M.id == 'W')
+        {
+            assert(M.numVals == 1);
+        }
+        if(M.id == 'L') //m_string.append("___");
+        {
+            assert(M.numVals == 0);
+            m_string.append("[ ]");
+        }
+        if(M.id == 'S') //m_string.push_back('-');
+        {
+            assert(M.numVals == 0);
+            m_string.append("[]");
+        }
+    }
+private:
+    std::string m_string;
+};
+
+int main(int argc, char **v)
+{
+    uint n = 10;
+    uint terminalWidth = 80;
+
+    if(argc == 2)
+    {
+        uint temp = std::stoul(v[1]);
+        if(temp != 0) terminalWidth = temp;
+    }
+
+    LSYSTEM::LSData lsData;
+    lsData.productions = 
+    {
+        "W(a) => W(a+1)",
+        "W(l) < L > W(r) : l >= r => SW(0)L",
+        "W(l) < L > W(r) : l < r => LW(0)S",
+        "S => L"
+    };
+
+    std::vector<std::string> S;
+    
+    try
+    {
+    LSYSTEM::LSystem ls(lsData);
+    CellTurtle C;
+    std::vector<LSYSTEM::VLSentence> A;
+    A.resize(n);
+    A[0] = "W(0)LW(1)";
+
+    ls.interpret(A[0],C);
+    S.emplace_back(std::move(C.getString()));
+    for(uint i = 1; i < n; ++i)
+    {
+        ls.iterate(A[i-1],A[i]);
+        ls.interpret(A[i],C);
+        S.emplace_back(std::move(C.getString()));
+    }
+
+    }
+    catch(std::exception &e)
+    {
+        std::cout<<e.what()<<"\n";
+        return 0;
+    }
+
+    for(auto &string : S)
+    {
+        uint offset = (terminalWidth - string.size()) / 2;
+        if(string.size() > terminalWidth) break;
+        for(uint i = 0; i < offset; ++i)
+            std::cout<<" ";
+        std::cout<<string<<"\n";
+    }
+
+    return 0;    
+}
