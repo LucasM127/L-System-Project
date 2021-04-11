664b56f8f8fc88cffc35d5d5e94ba8638d0bdf39
Lucas <what_is_hotmail@hotmail.com>
2021-04-11 12:14:37 -0600

    basic usage tests evaluator

--- Evaluator/test.cpp
+++ Evaluator/test.cpp
@@ -1,13 +1,94 @@
 //#include "EvalLoader.hpp"
-#include "../Containers/RPNToken.hpp"
-#include "../Containers/OstreamOperators.hpp"
+//#include "../Containers/RPNToken.hpp"
+//#include "../Containers/OstreamOperators.hpp"
 #include <iostream>
 #include <string>
-#include <cmath>
-#include "ShuntYardAlgorithm.hpp"
-#include "../Parsing/RPNTokenize.hpp"
+//#include <cmath>
+//#include "ShuntYardAlgorithm.hpp"
+//#include "../Parsing/RPNTokenize.hpp"
+#include "EvalLoader.hpp"
 
 #include <chrono>
+#include <cassert>
+
+
+//Usage
+//VarMap and global map both let the loader know which token ids are valid and what type they are (variable or global)
+//setGlobalMap before loading else won't know to look for those globals
+//varMap is set when load each expression, as can be different for each expression what each token means (not global but local)
+//setOffset in evalLoader called before loading each expression
+//offset = how much space to allocate in stack buffer for the variables to reside in and is used in creation of complex evaluators
+//then the buffer is generated with a call to getMaxStackSize so know how big a buffer needed to hold the variables, and stack op calculations
+//update is called after globals have been changed, so the complex evaluators can re-simplify for the new constant terms
+//generate doesn't really do anything. but if doing it with generating a dynamic lib file generates the file for libevaloader
+//same as init
+//multiplication is implied
+int main()
+{
+    VarIndiceMap varMap = 
+    {
+        {'x',{0,0}},
+        {'y',{1,0}}
+    };//Use a mapper function inside???? (needs depth)
+    std::unordered_map<char, float> globals =
+    {
+        {'g', 5.f},
+        {'h', 7.f}
+    };
+
+{
+    const std::string s1 = "5+x";
+    const std::string s2 = "5x^2y";
+    const std::string s3 = "-(x+1)(x-1)^2";
+    const std::string s4 = "x+1+g+(-3+y)";
+    const std::string s5 = "2max(4,2x)";
+    const std::string s6 = "randg";
+    const std::string s7 = "5x^(2y)";
+    //const std::string s8 = "5x^2y";
+    //const std::string s9 = "5x^2y";
+
+    EVAL::RuntimeLoader R;
+    R.init();
+    R.setGlobalMap(globals);
+    R.setOffset(2);
+
+    EVAL::Evaluator *e1 = R.load(s1, varMap, 1, "test1");
+    EVAL::Evaluator *e2 = R.load(s2, varMap, 1, "test2");
+    EVAL::Evaluator *e3 = R.load(s3, varMap, 1, "test3");
+    EVAL::Evaluator *e4 = R.load(s4, varMap, 1, "test4");
+    EVAL::Evaluator *e5 = R.load(s5, varMap, 1, "test5");
+    EVAL::Evaluator *e6 = R.load(s6, varMap, 1, "test6");
+    EVAL::Evaluator *e7 = R.load(s7, varMap, 1, "test7");
+    //EVAL::Evaluator *e8 = R.load(s8, varMap, 1, "test8");
+    //EVAL::Evaluator *e9 = R.load(s9, varMap, 1, "test9");
+
+    R.generate();
+    R.update();
+
+    std::vector<float> v(R.getMaxStackSz() + 2);
+    v[0] = 3.f;//x
+    v[1] = 4.f;//y
+    
+    float f1 = e1->evaluate(v.data());
+    assert(f1 == 8.f);
+    float f2 = e2->evaluate(v.data());//5x^2y
+    assert(f2 == 180.f);
+    float f3 = e3->evaluate(v.data());//-(x+1)(x-1)^2
+    assert(f3 == -16.f);
+    float f4 = e4->evaluate(v.data());//x+1+g+(-3+y)
+    assert(f4 == 10.f);//should be 3 pushes and 2 pops
+    float f5 = e5->evaluate(v.data());//2max(4,2x)
+    assert(f5 == 12.f);
+    float f6 = e6->evaluate(v.data());//randg
+    assert(f6 < globals['g']);//0-4
+    float f7 = e7->evaluate(v.data());//5x^(2y)
+    assert(f7 == 32805.f);
+}
+
+    return 0;
+}
+
+/*
 
 void findAndReplace(const EVAL::RPNList &subList, EVAL::RPNList &list)
 {
@@ -54,7 +135,7 @@ int main()
 
     return 0;
 }
-
+*/
 /*
 int main()
 {
